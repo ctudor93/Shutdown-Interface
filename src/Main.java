892154ece332150de9cf84646b60c12b357fb92c
ctudor93 <ctudor93@yahoo.com>
2023-03-05 15:14:36 +0200

    changed to Process Builder to run cmd

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String path="cmd /c start C:\\Users\\thudz\\IdeaProjects\\ShutdownGUI\\scripts\\Shutdown.bat";
-        Runtime rn=Runtime.getRuntime();
-        Process pr=rn.exec(path);
+        String path = "cmd /c start C:\\Users\\thudz\\IdeaProjects\\ShutdownGUI\\scripts\\";
+        String fileName ="Shutdown.bat";
 
-
+        ProcessBuilder builder = new ProcessBuilder(Arrays.asList("cmd.exe", "/C",path+fileName));
+        builder.start();
     }
 }
+
+
