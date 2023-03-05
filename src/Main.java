4a27933eb6672babd5ae96ce815c1fe284a8ed33
ctudor93 <ctudor93@yahoo.com>
2023-03-05 15:15:27 +0200

    changed path

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String path = "cmd /c start C:\\Users\\thudz\\IdeaProjects\\ShutdownGUI\\scripts\\";
+        String path = "C:\\Users\\thudz\\IdeaProjects\\ShutdownGUI\\scripts\\";
         String fileName ="Shutdown.bat";
 
         ProcessBuilder builder = new ProcessBuilder(Arrays.asList("cmd.exe", "/C",path+fileName));
