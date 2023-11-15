eba6ba11b06d2e36f8b68f5288a132a746ed4994
ctudor93 <ctudor93@yahoo.com>
2023-11-15 21:22:23 +0000

    rewrote config handler to actually write in file

--- src/main/ConfigHandler.java
+++ src/main/ConfigHandler.java
@@ -3,6 +3,8 @@ package main;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -11,15 +13,45 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
-    // (?<=\").*?(?=\") regex for anything between ""
-    //     (").*?(") better
+// (?<=\").*?(?=\") regex for anything between ""
+//     (").*?(") better
 
 public class ConfigHandler {
 
+    public static void writeToConfigTxt(String option, String stringToReplaceWith) {
 
+        System.out.println("received string in writeToConfigTxt " + stringToReplaceWith + " option " + option);
 
-        public static List<String> readConfig() {
+        String configFilePath = Paths.get("").toAbsolutePath() + "\\src\\Config\\Config.txt";
+        List<String> fileAsList = readConfig();
+
+        int lineInList = findLineForGivenOption(fileAsList, option);
+
+        fileAsList.set(lineInList, option + " = \"" + stringToReplaceWith + "\"");
+
+        // rewrite config file with new updated information
+        try {
+            Files.write(Paths.get(configFilePath), fileAsList);
+        } catch (IOException e) {
+            System.out.println("Problem reading or writing the file: " + e.getMessage());
+        }
+
+    }
+
+    // scan through list of lines from config file to find the line of the given
+    // option to be changed
+    public static int findLineForGivenOption(List<String> fileAsList, String option) {
+        for (int i = 0; i < fileAsList.size(); i++) {
+            if (fileAsList.get(i).contains(option))
+                return i;
+        }
+
+        return 0;
+    }
+
+    // create and return the config file as a string list
+
+    public static List<String> readConfig() {
 
         List<String> configs = new ArrayList();
         Path currentRelativePath = Paths.get("");
@@ -29,49 +61,18 @@ public class ConfigHandler {
             try (Scanner sc = new Scanner(configFile)) {
                 while (sc.hasNextLine()) {
 
-//                configs.add(sc.findInLine("(\").*?(\")"));
-                    //list where each entry is a line from the config file
+                    // list where each entry is a line from the config file
                     configs.add(sc.findInLine("(.).*"));
-                    if (sc.hasNextLine()) sc.nextLine();
+                    if (sc.hasNextLine())
+                        sc.nextLine();
                 }
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
 
-        System.out.println("this is main configs reader " + configs.toString());
+        System.out.println("this is readConfig() " + configs.toString());
         return configs;
-    }
-
-
-    public static void writeToConfigTxt(String option, String stringToReplaceWith) {
-        System.out.println("received string in writeToConfigTxt " + stringToReplaceWith +" option " + option);
-        String configFilePath = Paths.get("").toAbsolutePath() + "\\src\\Config\\Config.txt";
-        List<String> linesInFile = readConfig();
-
-        try {
-            for (int i = 0; i < linesInFile.size(); i++) {
-                if (linesInFile.get(i).matches("(" + option + ").*(\").*?(\")")) {
-                    linesInFile.add(i, linesInFile.get(i).replaceFirst("(\").*?(\")", stringToReplaceWith));
-                    System.out.println("Line in file "+linesInFile.get(i));
-                    break;
-                }
-            }
-
-            // write the new strings with the replaced lines OVER the same file
-            FileOutputStream fileOut = new FileOutputStream(configFilePath);
-            for (int i = 0; i < linesInFile.size(); i++) {
-                fileOut.write(linesInFile.get(i).getBytes());
-                fileOut.write("\n".getBytes());
-            }
-            fileOut.close();
-
-            System.out.println(readConfig());
-        } catch (Exception e) {
-            System.out.println("Problem reading file.");
-        }
-
-
     }
 
     public static String matchRegex(String stringToLookIn, String regexString) {
@@ -84,7 +85,5 @@ public class ConfigHandler {
         }
         return "";
     }
-
-
 
 }
